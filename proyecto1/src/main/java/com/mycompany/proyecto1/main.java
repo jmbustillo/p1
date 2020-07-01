b1ae5e3787716a792aef2a9670503e6a36828849
jmbustillo <67710929+jmbustillo@users.noreply.github.com>
2020-07-01 16:52:09 -0300

    Add files via upload

---
+++ proyecto1/src/main/java/com/mycompany/proyecto1/main.java
@@ -0,0 +1,20 @@
+package com.mycompany.proyecto1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class main {
+    
+    public static void main(String[] args) {
+         String nombre = "Juan";
+         int edad = 14;
+        System.out.println("mi nombre es "+ nombre);
+    }
+}
